08a0e8382810fdbbc8213e37e76f85aa5c29b315
Kovidh Meduri <kovidhmeduri@gmail.com>
2025-05-23 11:36:54 +0530

    init

--- src/a.js
+++ src/a.js
@@ -1,27 +1,178 @@
 /* eslint-disable */
 /*
 
-const person = {
-  firstName: 'john',
-  lastName: 'Doe',
-  age: 25,
+
+
+//Boolean
+let b = true;
+console.log('item --> ', typeof(b) );  
+//object
+let o1 = { a: 10 };
+console.log(x.a);
+
+let o2 = {
+  name: 'can',
+  element: 'steel'
 };
+console.log(x);
 
-let x = '10';
-console.log('item --> ', typeof(x) );
-
-const age = 20;
-if (age >= 18) {
-  console.log(true) ;
+//function
+function square(number) {
+  return number * number;
 }
 
-let x = { a: 10 };
-console.log(x.a);
+console.log(square(7));
+
+function outer() {
+  let counter = 0; //MA
+
+  let inner = () => {
+    counter = counter + 1;
+    return counter;
+  }
+
+  return inner;
+}
+
+let result = outer();
+
+
+console.log(result());
+console.log(result());
+
+
+
+//array
+
+let a = [10, 20, 30]
+console.log(a.length);
+
+
+
+
+// constructor
+function Person(k) {
+  this.name = k;
+  this.otherName = function () {
+    console.log('name --> ', this.name);
+  }
+}
+Person();
+
+let person = {
+  name: 'bomma',
+  car: {
+    colour: 'blue',
+    company: 'idk'
+  },
+};
+const newPerson = { ...person };
+
+newPerson.firstName = 'Mia';
+newPerson.car.color = 'red';
+console.log(person);
+console.log(newPerson);
+
+
+// primitives
+let n = 10;
+console.log(typeof (n));
+
+let s = '1'
+console.log(typeof (s));
+
+let t = true;
+console.log(typeof (b));
+
+let nu = null;
+console.log(typeof (nu));
+let un = undefined;
+console.log(typeof (un));
+
+// references
+
+let x = {
+  'name': 'fan',
+};
+console.log(typeof (x));
+let a2 = [
+  '10'
+];
+console.log(Array.isArray(a2));
 */
 
-let x={
-
+function add(number1, number2) {
+  return number1 + number2;
 };
+
+console.log(add(5, 3));
+
+
+const multiply = function (number1, number2) {
+  return number1 * number2
+}
+console.log(multiply(1, 2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
